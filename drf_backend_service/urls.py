95c85801cb477383403125c87d4bb1dad66d1f5d
Gurjot <singh13gurjot@gmail.com>
2020-08-03 11:58:42 +0530

    url modify for jwt

--- drf_backend_service/urls.py
+++ drf_backend_service/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('photos.api.urls')),
+    path('auth/jwt/', include('jwtauth.urls'), name='jwtauth'),
 ]
