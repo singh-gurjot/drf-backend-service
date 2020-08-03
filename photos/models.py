a1f6b60021adbbd86b7cf13088f9fcbf9b35ada3
Gurjot <singh13gurjot@gmail.com>
2020-08-03 11:42:18 +0530

    setup photos app in backend

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS_CHOICES = (
+    ("live", "live"),
+    ("draft", "draft"),
+)
+
+class Photo(models.Model):
+    publisher = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='./')
+    captions = models.CharField(max_length=255,null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=5, choices=STATUS_CHOICES, default='live')
+    
